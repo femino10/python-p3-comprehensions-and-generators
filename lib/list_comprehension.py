486e791cdd40304096eb13662ee4fd6723591c3c
felix <ndirangufelix10@gmail.com>
2023-11-30 17:23:00 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+# lib/sequences.py
 
-def make_exclamation(sentence_list):
+def return_evens(sequence):
+    return [num for num in sequence if num % 2 == 0]
+
+# Example usage:
+# Uncomment the line below to test the function with a sequence
+# result = return_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+# print(result)
+
+    
+
+# lib/sequences.py
+
+def make_exclamation(sentences):
+    return [sentence + '!' for sentence in sentences]
+
+# Example usage:
+# Uncomment the line below to test the function with a list of sentences
+# result = make_exclamation(["Hello", "How are you", "Goodbye"])
+# print(result)
+
     pass
